1806ae10e0b45ffdde271e70c08da0145f11b0f7
AdilMendubayev <AdilMendubayev@users.noreply.github.com>
2023-03-31 11:01:27 +0600

    f

--- src/TaskTwo.java
+++ src/TaskTwo.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class TaskTwo {
     public static void main(String[] args) {
-        System.out.println("ttt");
+        Scanner sc = new Scanner(System.in);
+        List<String> myList = new ArrayList<String>();
+        /*myList.add(sc.nextLine());
+        System.out.println(myList);
+        Set<String> toSet = new HashSet<>(myList);
+        System.out.println(toSet);
+        Set targetSet = Set.copyOf(myList);
+        System.out.println(targetSet);*/
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!");
+        Set<String> tSet = new HashSet<>();
+        tSet.add(sc.nextLine());
+        System.out.println(tSet);
     }
 }
+//List<Integer> sourceList = Arrays.asList(0, 1, 2, 3, 4, 5);
+//    Set<Integer> targetSet = new HashSet<>(sourceList);
